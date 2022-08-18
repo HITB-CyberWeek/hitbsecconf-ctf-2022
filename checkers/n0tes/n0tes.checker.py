303bf8e45ee7a506a130daf6da965244f2702670
Artem Zinenko <art@hackerdom.ru>
2022-08-18 22:11:00 +0500

    Added public_flag_description to n0tes checker

--- checkers/n0tes/n0tes.checker.py
+++ checkers/n0tes/n0tes.checker.py
@@ -3,13 +3,17 @@
 import sys
 import traceback
 import requests
+import random
+import string
+import json
 requests.packages.urllib3.disable_warnings()
 from checker_helper import *
 
 PORT = 8443
+TIMEOUT = 30
 
 def info():
-    verdict(OK, "vulns: 1\npublic_flag_description: Flag ID is a title of the note with flag")
+    verdict(OK, "vulns: 1\npublic_flag_description: Flag ID is a title of the note containing the flag")
 
 def check(args):
     if len(args) != 1:
@@ -19,12 +23,39 @@ def check(args):
 
     verdict(OK)
 
+def get_random_string(min_len, max_len):
+    letters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(letters) for i in range(random.randint(min_len, max_len)))
+
+def login(host, user, password):
+    n0tes_url = f"https://{host}:{PORT}/"
+
+    try:
+        r = requests.get(n0tes_url, timeout=TIMEOUT, verify=False)
+    except (requests.exceptions.ConnectionError, ConnectionRefusedError, http.client.RemoteDisconnected) as e:
+        return (DOWN, "Connection error", "Connection error during login: %s" % e, None)
+    except requests.exceptions.Timeout as e:
+        return (DOWN, "Timeout", "Timeout during login: %s" % e, None)
+
+    session = requests.Session()
+
+    return (OK, "", "", session)
+
 def put(args):
     if len(args) != 4:
         verdict(CHECKER_ERROR, "Checker error", "Wrong args count for put()")
-    host, flag_id, flag_data, vuln = args
-    trace("put(%s, %s, %s, %s)" % (host, flag_id, flag_data, vuln))
+    host, flag_id, flag, vuln = args
+    trace("put(%s, %s, %s, %s)" % (host, flag_id, flag, vuln))
 
+    note_title = flag_id
+    user = get_random_string(5, 15)
+    password = get_random_string(7, 20)
+
+    (status, out, err, session) = login(host, user, password)
+    if status != OK:
+        verdict(status, out, err)
+
+    flag_id = json.dumps({"public_flag_id": note_title, "user": user, "password": password})
     verdict(OK, flag_id)
 
 def get(args):
