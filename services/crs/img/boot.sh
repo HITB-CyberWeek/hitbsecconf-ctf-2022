d7ed5d474b1291b1ac0911e236949b078470aefc
dimmo <dimmoborgir@gmail.com>
2022-08-21 02:52:43 +0500

    [crs] better logging

--- services/crs/img/boot.sh
+++ services/crs/img/boot.sh
@@ -6,7 +6,9 @@ CPU="max,zpci=on"
 MEM=4096
 CORES=4
 
-qemu-system-s390x -machine s390-ccw-virtio -cpu $CPU -serial telnet::4441,server -display none -m $MEM \
+qemu-system-s390x -machine s390-ccw-virtio -cpu $CPU -m $MEM \
+    -chardev stdio,id=char0,mux=on,logfile=serial.log \
+    -serial chardev:char0 -mon chardev=char0 -display none \
     -drive format=qcow2,file=$IMAGE,cache=none,id=drive-virtio-disk0,if=none \
     -device virtio-blk-ccw,devno=fe.0.0001,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=1,scsi=off \
     -smp $CORES \
