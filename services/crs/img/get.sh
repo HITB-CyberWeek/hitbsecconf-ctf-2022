c419547946f398fbaf38430a143d5132c1061454
dimmo <dimmoborgir@gmail.com>
2022-08-21 09:26:26 +0500

    [crs] switch from jammy to focal in qemu image

--- services/crs/img/get.sh
+++ services/crs/img/get.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -eu
 
-URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-s390x.img"
-CACHE="/tmp/jammy-server-cloudimg-s390x.img"
+#URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-s390x.img"
+#CACHE="/tmp/jammy-server-cloudimg-s390x.img"
+#
+# User process fault: interruption code 0011 ilc:3
+# https://bugs.launchpad.net/ubuntu/+source/openssh/+bug/1970076
+
+URL="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-s390x.img"
+CACHE="/tmp/focal-server-cloudimg-s390x.img"
 
 if [ -f "$CACHE" ]; then
     echo "Cached file exists at '$CACHE'."
