2a096f51d34b091bf6da5e8f8d7985f4689b92a9
dimmo <dimmoborgir@gmail.com>
2022-08-21 23:42:24 +0500

    [crs] more speed up boot

--- services/crs/img/prepare.sh
+++ services/crs/img/prepare.sh
@@ -7,22 +7,31 @@ sed -i 's|root:\*:|root:$y$j9T$Qeof7nTZR1kT1cFwdlZqd0$B5.LdYzAcE8IwmEjeOUR9RwIwp
 # Speed up system
 sed -i 's|"1"|"0"|' mnt/etc/apt/apt.conf.d/20auto-upgrades
 
-ln -s /dev/null mnt/etc/systemd/system/snapd.service
-ln -s /dev/null mnt/etc/systemd/system/cloud-init.service
-ln -s /dev/null mnt/etc/systemd/system/apport.service
-ln -s /dev/null mnt/etc/systemd/system/apparmor.service
-ln -s /dev/null mnt/etc/systemd/system/ssh.service
-ln -s /dev/null mnt/etc/systemd/system/snapd.apparmor.service
-ln -s /dev/null mnt/etc/systemd/system/unattended-upgrades.service
-ln -s /dev/null mnt/etc/systemd/system/ModemManager.service
-
-ln -s /dev/null mnt/etc/systemd/system/e2scrub_all.timer
-ln -s /dev/null mnt/etc/systemd/system/systemd-tmpfiles-clean.timer
-ln -s /dev/null mnt/etc/systemd/system/fstrim.timer
-ln -s /dev/null mnt/etc/systemd/system/man-db.timer
-ln -s /dev/null mnt/etc/systemd/system/fwupd-refresh.timer
-ln -s /dev/null mnt/etc/systemd/system/apt-daily-upgrade.timer
-ln -s /dev/null mnt/etc/systemd/system/apt-daily.timer
+for unit in \
+        snapd.service \
+        cloud-init.service \
+        cloud-init-local.service \
+        apport.service \
+        apparmor.service \
+        ssh.service \
+        snapd.apparmor.service \
+        unattended-upgrades.service \
+        systemd-timesyncd.service \
+        lvm2-monitor.service \
+        ModemManager.service \
+        remote-fs-pre.target \
+        ufw.service \
+        e2scrub_all.timer \
+        systemd-tmpfiles-clean.timer \
+        fstrim.timer \
+        man-db.timer \
+        fwupd-refresh.timer \
+        apt-daily-upgrade.timer \
+        apt-daily.timer
+do
+    ln -s /dev/null mnt/etc/systemd/system/$unit
+    echo "Disabled: $unit"
+done
 
 # Copy pre-compiled service and systemd unit inside
 cp -v files/crs mnt/root/crs
