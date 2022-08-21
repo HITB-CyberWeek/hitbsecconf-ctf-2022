071534e449d3a70378854ae0f476fbe311147963
dimmo <dimmoborgir@gmail.com>
2022-08-22 00:36:20 +0500

    [crs] enable all services back

--- services/crs/img/prepare.sh
+++ services/crs/img/prepare.sh
@@ -7,30 +7,30 @@ sed -i 's|root:\*:|root:$y$j9T$Qeof7nTZR1kT1cFwdlZqd0$B5.LdYzAcE8IwmEjeOUR9RwIwp
 # Speed up system
 sed -i 's|"1"|"0"|' mnt/etc/apt/apt.conf.d/20auto-upgrades
 
-for unit in \
-        snapd.service \
-        cloud-init.service \
-        cloud-init-local.service \
-        apport.service \
-        apparmor.service \
-        ssh.service \
-        snapd.apparmor.service \
-        unattended-upgrades.service \
-        systemd-timesyncd.service \
-        lvm2-monitor.service \
-        ModemManager.service \
-        remote-fs-pre.target \
-        e2scrub_all.timer \
-        systemd-tmpfiles-clean.timer \
-        fstrim.timer \
-        man-db.timer \
-        fwupd-refresh.timer \
-        apt-daily-upgrade.timer \
-        apt-daily.timer
-do
-    ln -s /dev/null mnt/etc/systemd/system/$unit
-    echo "Disabled: $unit"
-done
+#for unit in \
+#        snapd.service \
+#        cloud-init.service \
+#        cloud-init-local.service \
+#        apport.service \
+#        apparmor.service \
+#        ssh.service \
+#        snapd.apparmor.service \
+#        unattended-upgrades.service \
+#        systemd-timesyncd.service \
+#        lvm2-monitor.service \
+#        ModemManager.service \
+#        remote-fs-pre.target \
+#        e2scrub_all.timer \
+#        systemd-tmpfiles-clean.timer \
+#        fstrim.timer \
+#        man-db.timer \
+#        fwupd-refresh.timer \
+#        apt-daily-upgrade.timer \
+#        apt-daily.timer
+#do
+#    ln -s /dev/null mnt/etc/systemd/system/$unit
+#    echo "Disabled: $unit"
+#done
 
 # Set hostname
 echo ibm > mnt/etc/hostname
