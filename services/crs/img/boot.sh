63a0cc27543d2458e8631b35a5d1f15db38e2ccc
dimmo <dimmoborgir@gmail.com>
2022-08-22 18:27:23 +0500

    [crs] change service port: 14444 -> 4444

--- services/crs/img/boot.sh
+++ services/crs/img/boot.sh
@@ -21,4 +21,4 @@ qemu-system-s390x -machine s390-ccw-virtio -cpu $CPU -m $MEM \
     -drive format=qcow2,file=$IMAGE,cache=none,id=drive-virtio-disk0,if=none \
     -device virtio-blk-ccw,devno=fe.0.0001,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=1,scsi=off \
     -smp $CORES \
-    -device virtio-net,netdev=vmnic -netdev user,id=vmnic,hostfwd=tcp::14444-:4444
+    -device virtio-net,netdev=vmnic -netdev user,id=vmnic,hostfwd=tcp::4444-:4444,hostfwd=tcp::22-:22
