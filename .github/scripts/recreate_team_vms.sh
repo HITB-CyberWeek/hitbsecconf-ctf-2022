38bbeb7fea389b65561fb8813bc63b08bf04bb65
Andrey Gein <andgein@yandex.ru>
2022-08-23 18:04:20 +0300

    [workflow] Add debug output to recreate_team_vms.sh

--- .github/scripts/recreate_team_vms.sh
+++ .github/scripts/recreate_team_vms.sh
@@ -7,6 +7,10 @@ SERVICE_NAME="$1"
 cd /cloud/backend
 service_id=$(grep -E ^$SERVICE_NAME\\s db/services.txt | awk '{print $2}')
 
+echo "Current 'init' images according to the cloud"
+ls -la do_vulnimages.json
+cat do_vulnimages.json
+
 for team in `seq 1 2`
 do
    ./restore_vm_from_snapshot.py $team $service_id init
