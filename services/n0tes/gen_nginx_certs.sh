4ad872aaa9a0d7d328577f33f3fdb7350cf94285
art <zinenkoartem@gmail.com>
2022-08-23 09:28:42 +0500

    Added nginx cert to n0tes checker

--- services/n0tes/gen_nginx_certs.sh
+++ services/n0tes/gen_nginx_certs.sh
@@ -6,6 +6,7 @@ ADMIN_HOST="admin.n0tes.ctf.hitb.org"
 if [ ! -f nginx/n0tes.key ] || [ ! -f nginx/n0tes.crt ] ; then
     echo "Generating self-signed certificate for $HOST and $ADMIN_HOST"
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx/n0tes.key -out nginx/n0tes.crt -subj "/CN=$HOST" -addext "subjectAltName = DNS:$ADMIN_HOST"
+    cp nginx/n0tes.crt ../../checkers/n0tes/
 fi
 
 if [ ! -f  ../../checkers/n0tes/n0tes-admin.key ] || [ ! -f ../../checkers/n0tes/n0tes-admin.crt ] ; then
