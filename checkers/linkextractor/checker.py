31cd27347efc9003d3edc7f796e6b90d8a49be21
Konstantin Plotnikov <kostteg@gmail.com>
2022-08-23 19:44:10 +0400

    linkextractor checker check+put+get

--- checkers/linkextractor/checker.py
+++ checkers/linkextractor/checker.py
@@ -8,15 +8,16 @@ import random
 import time
 import json
 import traceback
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote_plus, urlparse
 
 import requests
 
 OK, CORRUPT, MUMBLE, DOWN, CHECKER_ERROR = 101, 102, 103, 104, 110
 
 PORT = 80
-TIMEOUT = 10
+TIMEOUT = 10 #TODO use it ! :)
 
+#TODO
 #CHECKER_DIRECT_CONNECT = os.environ.get("CHECKER_DIRECT_CONNECT")
 
 def verdict(exit_code, public="", private=""):
@@ -33,56 +34,124 @@ def info():
 
 
 def gen_login():
-    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" #_$()'
+    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ_$()'"
     return "".join(random.choice(ABC) for i in range(random.randrange(6, 10)))
 
 
 def gen_password():
-    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" #_@$.,';:
+    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ_@$.,';:"
     return "".join(random.choice(ABC) for i in range(random.randrange(10, 16)))
 
 
-def gen_text():
-    return random.choice([gen_password(), gen_login(), gen_color(), "hello", "cccc", "#121212121"])
+def gen_description():
+    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    return "".join(random.choice(ABC) for i in range(random.randrange(2, 16)))
+
+def gen_domain_segment():
+    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ-"
+    LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return random.choice(LETTERS) + ("".join(random.choice(ABC) for i in range(random.randrange(2, 10))))
+
+def gen_path_segment():
+    ABC = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ-~_."
+    return "".join(random.choice(ABC) for i in range(random.randrange(1, 6)))
 
 
-def register_or_login_user(session, base_url, user, password):
+def call_register_or_login_user(session, base_url, user, password):
     register_data = {"login": user, "password": password}
-    try:
-        r = session.post(urljoin(base_url, "/users"), data=json.dumps(register_data), verify=False)
-    except (requests.exceptions.ConnectionError, ConnectionRefusedError) as e:
-        return (DOWN, "Connection error", "Connection error during registration or login: %s" % e)
-    except requests.exceptions.Timeout as e:
-        return (DOWN, "Timeout", "Timeout during registration or login: %s" % e)
-
+    r = session.post(urljoin(base_url, "/users"), data=json.dumps(register_data), verify=False)
     if r.status_code != 200:
-        return (MUMBLE, "Can't register or login", "Unexpected register or login result: '%d' -> %s" %(r.status_code, r.text))
+        verdict(MUMBLE, "Can't register or login", "Unexpected register or login result on cred '%s': %d -> %s" %(register_data, r.status_code, r.text))
 
-    return (OK, "", "")
+def call_get_pages(session, base_url):
+    r = session.get(urljoin(base_url, "/pages"))
+    if r.status_code != 200:
+        verdict(MUMBLE, "Can't GET pages", "Unexpected GET /pages result: %d -> %s" %(r.status_code, r.text))
+
+    try:
+        return r.json()
+    except Exception:
+        verdict(MUMBLE, "Bad json in GET pages result", "Bad json in GET /pages result: %s (exception %s)" % (r.text, traceback.format_exc()))
+
+def call_get_page(session, base_url, page_id):
+    r = session.get(urljoin(base_url, f"/pages/{page_id}"))
+    if r.status_code != 200:
+        verdict(MUMBLE, f"Can't GET page {page_id}", "Unexpected GET /pages/%d result: %d -> %s" %(page_id, r.status_code, r.text))
+
+    try:
+        return r.json()
+    except Exception:
+        verdict(MUMBLE, f"Bad json in GET page {page_id} result", "Bad json in GET /pages/%d result: %s (exception %s)" % (page_id, r.text, traceback.format_exc()))
+
+def call_parse_page(session, base_url, page_url, text):
+    r = session.post(urljoin(base_url, "/pages/?url=%s" % quote_plus(page_url)), data=text)
+    if r.status_code != 200:
+        verdict(MUMBLE, "Can't POST page to parse", "Unexpected POST page with url '%s' and text '%s' to parse result: %d -> %s" %(page_url, text, r.status_code, r.text))
+    try:
+        return r.json()
+    except Exception:
+        verdict(MUMBLE, f"Bad json in POST page to parse result", "Bad json in POST page with url '%s' and text '%s' to parse result: %s (exception %s)" % (page_url, text, r.text, traceback.format_exc()))
+
+def gen_page(page_url):
+    links = []
+    for i in range(random.randrange(1, 8)):
+        choice = random.random()
+        if choice < 0.2:
+            links.append(gen_absolute_url())
+        elif choice < 0.4:
+            links.append(gen_absolute_url_preserving_protocol())
+        elif choice < 0.6:
+            links.append(gen_relative_url_from_root())
+        elif choice < 0.8:
+            links.append(gen_relative_url_upper())
+        else:
+            links.append(gen_relative_url())
+        #TODO generate duplicates in different forms
+
+    html = "<html>\n"
+    for link in links:
+        description = gen_description()
+        html += f"<a href=\"{link}\">{description}</a>\n"
+
+    html += "</html>"
+    return (html, links)
+
+def gen_absolute_url():
+    proto = random.choice(["http", "https"])
+    domain = ".".join(gen_domain_segment() for i in range(random.randrange(2,4)))
+    path = "/".join(gen_path_segment() for i in range(random.randrange(0,5)))
+    return f"{proto}://{domain}/{path}"
+
+def gen_absolute_url_preserving_protocol():
+    domain = ".".join(gen_domain_segment() for i in range(random.randrange(2,4)))
+    path = "/".join(gen_path_segment() for i in range(random.randrange(0,5)))
+    return f"//{domain}/{path}"
+
+def gen_relative_url_from_root():
+    path = "/".join(gen_path_segment() for i in range(random.randrange(0,5)))
+    return f"/{path}"
+
+def gen_relative_url_upper():
+    upperLevels = "/".join(".." for i in range(random.randrange(1,5)))
+    path = "/".join(gen_path_segment() for i in range(random.randrange(0,5)))
+    return f"{upperLevels}/{path}"
+
+def gen_relative_url():
+    return "/".join(gen_path_segment() for i in range(random.randrange(0,5)))
 
 
+#TODO random small timeouts between requests
 def check(host):
-    base_url = f"http://{host}:{PORT}/"
+    linkextractor_base_url = f"http://{host}:{PORT}/"
     session = requests.Session()
-
     login = gen_login()
     password = gen_password()
-    (status, out, err) = register_or_login_user(session, base_url, login, password)
-    if status != OK:
-        verdict(status, out, err)
+    call_register_or_login_user(session, linkextractor_base_url, login, password)
 
-    if random.random() < 0.5:
-        (status, out, err) = register_or_login_user(session, base_url, login, password)
-        if status != OK:
-            verdict(status, out, err)
+    if random.random() < 0.65:
+        call_register_or_login_user(session, linkextractor_base_url, login, password)
 
-
-    try:
-        r = session.get(urljoin(base_url, "/users/whoami"))
-    except (requests.exceptions.ConnectionError, ConnectionRefusedError) as e:
-        verdict(DOWN, "Connection error", "Connection error during whoami: %s" % e, None)
-    except requests.exceptions.Timeout as e:
-        verdict(DOWN, "Timeout", "Timeout during whoami: %s" % e, None)
+    r = session.get(urljoin(linkextractor_base_url, "/users/whoami"))
 
     if r.status_code != 200 or r.text != login:
         verdict(MUMBLE, "Can't check user", "Unexpected whoami result: '%d' -> %s" %(r.status_code, r.text))
@@ -90,12 +159,58 @@ def check(host):
     verdict(OK)
 
 def put(host, flag_id, flag, vuln):
-    raise Exception("put not implemented")
-    
+    linkextractor_base_url = f"http://{host}:{PORT}/"
+    session = requests.Session()
+    login = gen_login()
+    password = gen_password()
+    call_register_or_login_user(session, linkextractor_base_url, login, password)
 
+    page_url = "https://" + flag
+    (page_content, links) = gen_page(page_url)
+    page_model = call_parse_page(session, linkextractor_base_url, page_url, page_content)
+
+
+    try:
+        parsed_pageId = int(page_model.get("pageId"))
+        parsed_linksCount = int(page_model.get("linksCount"))
+        parsed_pageUrl = page_model.get("pageUrl")
+
+        if parsed_pageUrl != page_url:
+            verdict(MUMBLE, f"Invalid page url returned in result model from parse page request", "Invalid page url returned in result model from parse page request '%s' with content '%s': %s" % (page_url, page_content, json.dumps(page_model)))
+
+        #TODO check distinctness count or resulting links on page
+        if parsed_linksCount < 0:
+            verdict(MUMBLE, f"Invalid links count parsed from page", "Invalid links count parsed from page '%s' with content '%s': %s" % (page_url, page_content, json.dumps(page_model)))
+    except Exception:
+        verdict(MUMBLE, f"Invalid model from successful result of parse page request", "Invalid model from successful result of parse page request '%s' with content '%s': %s (exception %s)" % (page_url, page_content, json.dumps(page_model), traceback.format_exc()))
+
+    verdict(OK, json.dumps({"public_flag_id": str(parsed_pageId), "login": login, "password": password}))
 
 def get(host, flag_id, flag, vuln):
-    raise Exception("put not implemented")
+    linkextractor_base_url = f"http://{host}:{PORT}/"
+    session = requests.Session()
+
+    try:
+        info = json.loads(flag_id)
+        page_id = info["public_flag_id"]
+        login = info["login"]
+        password = info["password"]
+    except Exception:
+        verdict(CHECKER_ERROR, "Bad flag id", "Bad flag_id: %s" % traceback.format_exc())
+
+    call_register_or_login_user(session, linkextractor_base_url, login, password)
+
+    page_model = call_get_page(session, linkextractor_base_url, page_id)
+    if "pageUrl" not in page_model:
+        verdict(MUMBLE, "Failed to get page", "Failed to get page %s: %s %s" % (page_id, login, password))
+
+    page_url = page_model["pageUrl"]
+    expected_page_url = "https://" + flag
+
+    if page_url != expected_page_url:
+        verdict(CORRUPT, "Flag not found", "Flag not found expected %s, got %s" %(expected_page_url, page_url))
+
+    verdict(OK)
 
 def main(args):
     CMD_MAPPING = {
@@ -114,7 +229,7 @@ def main(args):
 
     handler, args_count = CMD_MAPPING[cmd]
     if len(args) != args_count:
-        verdict(CHECKER_ERROR, "Checker error", "Wrong args count for %s" % cmd)
+        verdict(CHECKER_ERROR, "Checker error", "Wrong args count for %s, expected %d got %d" % (cmd, args_count, len(args)))
 
     try:
         handler(*args)
