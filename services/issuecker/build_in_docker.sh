ab1193d85354940396e3ce6800c2ac67a7a43c59
Andrey Gein <andgein@yandex.ru>
2022-08-24 16:22:58 +0300

    [issuecker] Fix bug in build_in_docker.sh

--- services/issuecker/build_in_docker.sh
+++ services/issuecker/build_in_docker.sh
@@ -3,4 +3,4 @@ set -ex
 docker build -t issuecker.build .
 docker run -v "${PWD}/build:/app/build" issuecker.build
 
-sudo chmod -R $(id -nu):$(id -ng) "${PWD}/build"
+sudo chown -R $(id -nu):$(id -ng) "${PWD}/build"
