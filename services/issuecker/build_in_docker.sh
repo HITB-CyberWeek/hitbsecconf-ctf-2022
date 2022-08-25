b7d236dfe971771157bc1be518301a30870cbc6c
Daniil Sharko <integrex@yandex.ru>
2022-08-25 20:27:54 +0300

    Fix build_in_docker.sh

--- services/issuecker/build_in_docker.sh
+++ services/issuecker/build_in_docker.sh
@@ -1,6 +1,6 @@
 set -ex
 
 docker build -f Dockerfile.build -t issuecker.build .
-docker run --rm -v "${PWD}/server:/app/server" -it issuecker.build
+docker run --rm -v "${PWD}/server:/app/server" issuecker.build
 
 sudo chown -R "$(id -nu):$(id -ng)" "${PWD}/server"
