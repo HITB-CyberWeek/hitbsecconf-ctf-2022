e591ef90a109a88dd5f8f9ecc6487a406b864b3e
Andrey Gein <andgein@yandex.ru>
2022-08-25 18:01:47 +0300

    [deploy_proxies] Support $TEAM_ID

--- vuln_images/deploy_all_proxies.sh
+++ vuln_images/deploy_all_proxies.sh
@@ -2,16 +2,22 @@
 
 set -e
 
+team_filter=""
+if [[ ! -z "$TEAM_ID" ]]
+then
+    team_filter="--team-id $TEAM_ID"
+fi
+
 prepared=0
 for deploy_yaml in ../services/*/deploy.yaml
 do
   # We need to run preparation steps only once
   if [[ $prepared == 0 && $1 != "--skip-preparation" ]]
   then
-    ./deploy_proxies.py --prepare-only "$deploy_yaml"
+    ./deploy_proxies.py --prepare-only $team_filter "$deploy_yaml"
     prepared=1
   fi
 
   # Skip preparation (because it's done already) and deploy proxies!
-  ./deploy_proxies.py --skip-preparation --skip-dns "$deploy_yaml"
+  ./deploy_proxies.py --skip-preparation --skip-dns $team_filter "$deploy_yaml"
 done
