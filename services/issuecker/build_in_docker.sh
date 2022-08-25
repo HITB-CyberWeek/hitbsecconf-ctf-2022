77b4f058ddd62e5dfc6369c424882d684d16674a
Daniil Sharko <integrex@yandex.ru>
2022-08-25 19:33:29 +0300

    Add swagger

--- services/issuecker/build_in_docker.sh
+++ services/issuecker/build_in_docker.sh
@@ -1,6 +1,6 @@
 set -ex
 
 docker build -f Dockerfile.build -t issuecker.build .
-docker run -v "${PWD}/build:/app/build" issuecker.build
+docker run --rm -v "${PWD}/server:/app/server" -it issuecker.build
 
-sudo chown -R "$(id -nu):$(id -ng)" "${PWD}/build"
+#sudo chown -R "$(id -nu):$(id -ng)" "${PWD}/server"
